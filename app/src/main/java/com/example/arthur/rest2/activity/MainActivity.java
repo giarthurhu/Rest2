e1edf9d77cb664de93549dfb6b27454675196b5d
giarthurhu <arthurhu@hufamily.com>
2018-07-20 18:59:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/arthur/rest2/activity/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.arthur.rest2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.example.arthur.rest2.R;
+import com.example.arthur.rest2.adapter.MoviesAdapter;
+import com.example.arthur.rest2.model.Movie;
+import com.example.arthur.rest2.model.MovieApiService;
+import com.example.arthur.rest2.model.MovieResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+// this is based on https://android.jlelse.eu/consuming-rest-api-using-retrofit-library-in-android-ed47aef01ecb
+// with minor tweaks for 7/2018
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    private static Retrofit retrofit = null;
+    private RecyclerView recyclerView = null;
+    // insert your themoviedb.org API KEY here
+    private final static String API_KEY = "7b71ac82f2a555685d9f23e710eefe0b";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // dies
+        connectAndGetApiData();
+    }
+    // This method create an instance of Retrofit
+// set the base url
+    public void connectAndGetApiData(){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        //dies
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+        Call<MovieResponse> call = movieApiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+                Log.d(TAG, "Number of movies received: " + movies.size());
+            }
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
+    }
+}
